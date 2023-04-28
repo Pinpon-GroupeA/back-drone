9266d80778b4a683350d35d591f38708375f98ab
damienMS <91212930+damienMS@users.noreply.github.com>
2023-04-28 16:22:23 +0200

    feat: add command to do opened/closed circuit
    feat: add command to do opened/closed circuit

--- drone/drone-actions.py
+++ drone/drone-actions.py
@@ -1,4 +1,3 @@
-import asyncio
 from tkinter import *
 from turtle import home
 from async_tkinter_loop import async_handler, async_mainloop
@@ -17,6 +16,7 @@ def hyperLink(url):
 
 
 connected = False
+stop_drone = False
 
 
 async def setup(drone):
@@ -108,11 +108,66 @@ async def goto_coordinates(drone, coordinates):
     await drone.mission.start_mission()
 
 
+async def goto_coordonnates_close(drone, coordinates):
+    """
+    Moves the drone to the specified coordinates.
+    :param coordinates: List of dictionaries containing the latitude, longitude, and altitude of the coordinates.
+    """
+    global finish
+    if not connected:
+        await setup(drone)
+    finish = True
+    while (float(await get_battery(drone)) > 0.1 and not stop_drone):
+        printPxh(f"stop_drone: {stop_drone}")
+        printPxh(f"finish: {finish}")
+        if finish:
+            finish = False
+            await goto_coordinates(drone, coordinates)
+        finish = bool(await drone.mission.is_mission_finished())
+
+
+async def go_to_coordinates_open(drone, coordinates):
+    """
+    Moves the drone to the specified coordinates.
+    :param coordinates: List of dictionaries containing the latitude, longitude, and altitude of the coordinates.
+    """
+    global finish
+    global reversed
+    reverse_coordinates = coordinates[::-1]
+    reversed = False
+    if not connected:
+        await setup(drone)
+    finish = True
+    while (float(await get_battery(drone)) > 0.1 and not stop_drone):
+        printPxh(f"finish: {finish}")
+        if finish:
+            if reversed:
+                reversed = False
+                await goto_coordinates(drone, reverse_coordinates)
+            else:
+                reversed = True
+                await goto_coordinates(drone, coordinates)
+            finish = False
+        finish = bool(await drone.mission.is_mission_finished())
+
+
+async def get_battery(drone):
+    """
+    Gets the battery level of the drone.
+    """
+    printPxh("-- Getting battery level")
+    async for battery in drone.telemetry.battery():
+        printPxh(f"-- Battery: {battery.remaining_percent}")
+        return battery.remaining_percent
+
+
 async def return_to_home(drone):
     """
     Returns the drone to the takeOff position.
     """
+    printPxh("-- Returning to home")
     await drone.action.return_to_launch()
+    printPxh("-- arrived at home")
 
 
 def printPxh(message=""):
